844cc02ce317718b766689d91dbf765931c81cdc
Daniel Antonio Vargas Rosales <antonio.vargasrosales@gmail.com>
2024-08-11 02:32:50 -0600

    :sparkles: Feat: add new component for Login view
    Also was modify the routes to be able to access the new view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('src/app/views/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+  },
+  { path: '**', redirectTo: 'login' },
+];
