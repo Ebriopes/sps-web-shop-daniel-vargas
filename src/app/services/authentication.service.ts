276f77c897c5a7a8f77e188f2878519321f96434
Daniel Antonio Vargas Rosales <antonio.vargasrosales@gmail.com>
2024-08-11 23:43:05 -0600

    :alien: Feature: create request for login and register

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,26 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICredentials, ILogData } from '@models/credentials.model';
+import { IUser } from '@models/user.model';
 
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
   private readonly isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  private baseUrl = 'https://fakestoreapi.com';
 
   get isAuthenticated$() {
     return this.isAuthenticatedSubject.asObservable();
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  login() {
-    this.isAuthenticatedSubject.next(true);
-    console.log('logged in');
+  register(data: ILogData): Observable<IUser> {
+    return this.http.post<IUser>(`${this.baseUrl}/users`, data);
+  }
+
+  login(credentials: ICredentials): Observable<string> {
+    return this.http.post<any>(`${this.baseUrl}/auth/login`, credentials).pipe(
+      map((response) => {
+        this.isAuthenticatedSubject.next(true);
+
+        return response;
+      })
+    );
   }
 
   logout() {
+    sessionStorage.removeItem('token');
+    // Redirigir al usuario a la página de inicio de sesión
     this.isAuthenticatedSubject.next(false);
-    console.log('logged out');
   }
 }
